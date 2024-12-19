876833087adb406b9643a3f7cf3f27216bbe8afc
Soyeon <111319810+ssoyeee@users.noreply.github.com>
2024-12-18 19:26:15 -0500

    LC-minimum-number-of-food-buckets-to-feed-the-hamsters [Runtime: 19 ms; Memory: 17.9 MB]

--- greedy/minimum-number-of-food-buckets-to-feed-the-hamsters.py
+++ greedy/minimum-number-of-food-buckets-to-feed-the-hamsters.py
@@ -13,14 +13,28 @@ class Solution:
     def minimumBuckets(self, hamsters: str) -> int:
         n = len(hamsters)
         i, result = 0, 0
+        # loop through each char in the hamsters
         while i < n:
+            # if we encounter 'H'amster
             if hamsters[i] == 'H':
+                # check if there's empty indices for food buckets 
+                # and avoid out of range error
                 if i+1 < n and hamsters[i+1] == '.':
+                    # skip the next, move i to i+2, as we place a bucket here at i+1 
+                    # i+1 potentially can also feed a ham at i+2
                     i += 2
+                    # increment bucket count
                     result += 1
+                # if no space on the right, check for space on the left
                 elif i- 1 >= 0 and hamsters[i-1] == '.':
+                    # no need to move i by 2 
                     result += 1
+                # no place to put, return -1
                 else:
                     return -1
+            # if current char is not a hams move to next the index
             i += 1
         return result
+    # Time: O(n), In worst, every char is visited once, and update operations are O(1) 
+                # ->loop iterates at most n times, where n is len(hamsters) 
+    # Space: O(1), only uses a fixed num of variables (result, i, n)
