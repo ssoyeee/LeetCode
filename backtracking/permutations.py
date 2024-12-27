4b571b5ad61f9eac11f74af0beba0ed5b5071e70
Soyeon <111319810+ssoyeee@users.noreply.github.com>
2024-12-27 18:19:44 -0500

    LC-permutations [Runtime: 3 ms; Memory: 17.9 MB]

--- backtracking/permutations.py
+++ backtracking/permutations.py
@@ -2,4 +2,10 @@ from itertools import permutations
 
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        return list(permutations(nums))
+        def helper(prefix, nums):
+            if not nums: 
+                yield prefix
+                return
+            for index, num in enumerate(nums):
+                yield from helper(prefix + [num], nums[:index] + nums[index+1:])
+        return list(helper([], nums))
