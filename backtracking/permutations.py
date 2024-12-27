d3401bd3d531d1c35a40157f06ec865757879427
Soyeon <111319810+ssoyeee@users.noreply.github.com>
2024-12-27 18:10:57 -0500

    LC-permutations [Runtime: 0 ms; Memory: 17.8 MB]

--- backtracking/permutations.py
+++ backtracking/permutations.py
@@ -1,3 +1,5 @@
+from itertools import permutations
+
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        return itertools.permutations(nums)
+        return list(permutations(nums))
