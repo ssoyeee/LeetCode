01ad317e7babf4b63f5c5d54db28c77c98ad1b32
Soyeon <111319810+ssoyeee@users.noreply.github.com>
2025-01-01 20:14:06 -0500

    LC-number-of-connected-components-in-an-undirected-graph [Runtime: N/A; Memory: N/A]

--- union-find/number-of-connected-components-in-an-undirected-graph.py
+++ union-find/number-of-connected-components-in-an-undirected-graph.py
@@ -1,24 +1,22 @@
 class Solution:
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
-        
         def dfs(index):
-            visited[index] = True
+            if not visited and graph[index][i]:
+                visited[index] = True
+                answer += 1
             
-            for i in range(n):
-                if not visited[i] and graph[index][i]:
-                    dfs(i)
-
-        graph = [[False] * (n) for _ in range(n)]
-        visited = [False] * (n)
+       # Input and initialize
+        graph = [[False] * (n+1) for _ in range(n+1)]
+        visited = [False] * (n+1)
         answer = 0
-
+        
+        # fill out connected components
         for u, v in edges:
             graph[u][v] = True
             graph[v][u] = True
         
-        for i in range(n):
-            if not visited[i]:
-                dfs(i)
-                answer += 1
-        
-        return answer
+        # call dfs
+        for i in range(1, n+1):
+            dfs(i)
+
+        return answer-1
