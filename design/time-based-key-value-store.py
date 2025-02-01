de6435d20222d99b8e1dd5e38197272a89b5f4c3
Soyeon <111319810+ssoyeee@users.noreply.github.com>
2025-01-31 20:51:13 -0500

    LC-time-based-key-value-store [Runtime: N/A; Memory: N/A]

--- design/time-based-key-value-store.py
+++ design/time-based-key-value-store.py
@@ -5,19 +5,29 @@ class TimeMap:
     def set(self, key: str, value: str, timestamp: int) -> None:
         if not key in self.key_time_map:
             self.key_time_map[key] = {}
-        self.key_time_map[key][timestamp] = value
-        
 
+        self.key_time_map[key].append([ timestamp, value ])
+
+        
     def get(self, key: str, timestamp: int) -> str:
         if not key in self.key_time_map:
             return ""
-        
-        for curr_time in reversed(range(1, timestamp + 1)):
-            if curr_time in self.key_time_map[key]:
-                return self.key_time_map[key][curr_time]
-            
-        return ""
 
+        if timestamp < self.key_time_map[key][0][0]:
+            return ""
+                
+            left = 0
+            right = len(self.key_time_map[key])
+                
+            while left < right:
+                mid = (left + right) // 2
+                if self.key_time_map[key][mid][0] <= timestamp:
+                    left = mid + 1
+                else:
+                    right = mid
+
+                # If iterator points to first element it means, no time <= timestamp exists.
+            return "" if right == 0 else self.key_time_map[key][right - 1][1]
 
 # Your TimeMap object will be instantiated and called as such:
 # obj = TimeMap()
