3770667172523fc6a78aa27ff2d3cdd69f3b8a53
Soyeon <111319810+ssoyeee@users.noreply.github.com>
2025-02-05 23:59:17 -0500

    LC-rotting-oranges [Runtime: 3 ms; Memory: 17.7 MB]

--- breadth-first-search/rotting-oranges.py
+++ breadth-first-search/rotting-oranges.py
@@ -5,22 +5,12 @@ class Solution:
         fresh = {(i, j) for i in range(row) for j in range(col) if grid[i][j] == 1}
         timer = 0
         new_dir = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-
         while fresh:
-            if not rotten:
+            if not rotten: 
                 return -1
-            
-            new_rotten = set()
-            for i, j in rotten:
-                for di, dj in new_dir:
-                    ni, nj = i + di, j + dj
-                    if (ni, nj) in fresh:
-                        new_rotten.add((ni, nj))
-        
-        fresh -= new_rotten
-        rotten = new_rotten
-        timer += 1
-
+            rotten = {(i+di, j+dj) for i, j in rotten for di, dj in new_dir if (i+di, j+dj) in fresh}
+            fresh -= rotten
+            timer += 1
         return timer
 
         # Time O(m*n) where m, n are the number of rows and col
