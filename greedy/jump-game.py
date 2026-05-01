95dd8fe9acd4e66ecc46119a2f8e3ef100eda739
Soyeon <111319810+ssoyeee@users.noreply.github.com>
2026-05-01 00:17:31 -0400

    LC-jump-game [Runtime: 17 ms; Memory: 20.3 MB]

--- greedy/jump-game.py
+++ greedy/jump-game.py
@@ -1,12 +1,8 @@
 class Solution(object):
     def canJump(self, nums):
-        GOOD, BAD, UNKNOWN = 1, 0, -1
-        memo = [UNKNOWN] * len(nums)
-        memo[-1] = GOOD
-        for i in range(len(nums) - 2, -1, -1):
-            furthest_jump = min(i + nums[i], len(nums) - 1)
-            for j in range(i + 1, furthest_jump + 1):
-                if memo[j] == GOOD:
-                    memo[i] = GOOD
-                    break
-        return memo[0] == GOOD
+        max_reach = 0
+        for i in range(len(nums)):
+            if i > max_reach:
+                return False
+            max_reach = max(max_reach, nums[i] + i)
+        return True
