5e25c4d0f7ce257d09dad6b2a9073a797cb1b3a5
Soyeon <111319810+ssoyeee@users.noreply.github.com>
2026-05-01 16:39:50 -0400

    LC-sort-characters-by-frequency [Runtime: 7 ms; Memory: 21.9 MB]

--- string/sort-characters-by-frequency.py
+++ string/sort-characters-by-frequency.py
@@ -1,10 +1,28 @@
 class Solution:
     def frequencySort(self, s: str) -> str:
         counts = Counter(s)
+        buckets = [""] * (len(s) + 1)
+    
+        for val, freq in counts.items():
+            buckets[freq] += val * freq
+
+        return "".join(reversed(buckets))
+        
+        # bucket sort
+        # T: O(N)
+        # S: O(N)
+        # Trade-off: uses memory for speed
+        # Note: bucket size is bounded by len(s), 
+        # but since input is alphabet only, at most 26 unique characters
+        '''
+        counts = Counter(s)
         sorted_counts = sorted(counts.items(), key=lambda x: (-x[1], x[0]))
         
         return "".join(val * freq for val, freq in sorted_counts)
 
-        # T: O(N log N) where N = unique characters
+        # Sorted
+        # T: O(N log N) where N is unique chars
         # S: O(N)
-            
+        # Trade-off: uses speed for memory
+        # Note: readability over speed (O(N log N) but simpler code)
+        '''
