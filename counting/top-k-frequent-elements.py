71dda02dfae8b44c6836467aed7cea5d5cd55326
Soyeon <111319810+ssoyeee@users.noreply.github.com>
2026-05-01 16:03:38 -0400

    LC-top-k-frequent-elements [Runtime: 12 ms; Memory: 14.5 MB]

--- counting/top-k-frequent-elements.py
+++ counting/top-k-frequent-elements.py
@@ -7,7 +7,7 @@ class Solution(object):
         :type k: int
         :rtype: List[int]
         """
-        
         counts = Counter(nums)
         sorted_counts = sorted(counts.items(), key=lambda x: (-x[1], x[0]))
         return [val for val, freq in sorted_counts[:k]]
+        
