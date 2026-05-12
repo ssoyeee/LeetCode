14b35f9fec9739de677bb47be6caf543ca724e59
Soyeon <111319810+ssoyeee@users.noreply.github.com>
2026-05-11 23:59:03 -0400

    LC-reward-top-k-students [Runtime: N/A; Memory: N/A]

---
+++ array/reward-top-k-students.py
@@ -0,0 +1,14 @@
+class Solution:
+    def topStudents(self, positive_feedback: List[str], negative_feedback: List[str], report: List[str], student_id: List[int], k: int) -> List[int]:
+        stud = []
+        pos, neg = set(pos), set(neg)
+    
+        for rep, sid in zip(report, student_id):
+            score = 0
+            for w in rep.split(" "):
+                if w in pos : score += 3
+                if w in neg : score -= 1
+            stud.append((-score,sid))
+            
+        stud.sort()
+        return [sid for _, sid in stud[0:k]]
