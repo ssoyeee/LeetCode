2aa67ea44a5f23c87ec434b0b7a77c773deda730
Soyeon <111319810+ssoyeee@users.noreply.github.com>
2026-05-15 14:57:58 -0400

    LC-root-equals-sum-of-children [Runtime: N/A; Memory: N/A]

--- binary-tree/root-equals-sum-of-children.py
+++ binary-tree/root-equals-sum-of-children.py
@@ -7,3 +7,6 @@
 class Solution:
     def checkTree(self, root: Optional[TreeNode]) -> bool:
         return root.val == root.left.val + root.right.val
+
+        #T: O(1) -- 3 nodes always
+        #S: O(1) -- no extra space
