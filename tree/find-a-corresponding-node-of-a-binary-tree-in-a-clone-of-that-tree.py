fd803760cf087bf4fb7f9e7b28e06987407e468d
Soyeon <111319810+ssoyeee@users.noreply.github.com>
2026-05-15 15:10:39 -0400

    LC-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree [Runtime: N/A; Memory: N/A]

--- tree/find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.py
+++ tree/find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.py
@@ -7,9 +7,13 @@
 
 class Solution:
     def getTargetCopy(self, original: TreeNode, cloned: TreeNode, target: TreeNode) -> TreeNode:
+        # base case: empty tree or reached beyond leaf node
         if original is None: 
             return None
         if original is target:
             return cloned
         
         return self.getTargetCopy(original.left, cloned.left, target) or self.getTargetCopy(original.right, cloned.right, target)
+
+        #S: O(H) where H is height of tree
+        #T: O(N) where N is number of nodes
