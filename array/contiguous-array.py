4c5171516051bdb5ec4f6f7b8adfeb34e7af252c
Soyeon <111319810+ssoyeee@users.noreply.github.com>
2026-06-09 23:33:28 -0400

    LC-contiguous-array [Runtime: N/A; Memory: N/A]

--- array/contiguous-array.py
+++ array/contiguous-array.py
@@ -1,13 +1,19 @@
 class Solution:
     def findMaxLength(self, nums: List[int]) -> int:
+        seen = {0: -1}
         max_len = 0
-        for i in range(len(nums)):
-            zero, one = 0, 0
-            for j in range(i, len(nums)):
-                if nums[j] == 0:
-                    zero += 1
-                else:
-                    one += 1
-                if zero == one:
-                    max_len = max(max_len, j-i+1)
+        prefix_sum = 0
+
+        for i, num in enumerate(nums):
+            if num == 0:
+                prefix_sum -= 1
+            else:
+                prefix_sum += 1
+            if prefix_sum in seen:
+                max_len = max(max_len, i - seen[prefix_sum])
+            else: 
+                seen[prefix_sum] = i
         return max_len
+        
+
+
