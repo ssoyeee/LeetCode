c226978dff5ab140d636645a8321ecd93bf01924
Soyeon <111319810+ssoyeee@users.noreply.github.com>
2026-06-10 03:30:46 -0400

    LC-roman-to-integer [Runtime: 16 ms; Memory: 12.5 MB]

--- string/roman-to-integer.py
+++ string/roman-to-integer.py
@@ -4,7 +4,7 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        roman = {'I': 1, ' V': 5, 'X': 10,  'L': 50, 'C':  100, 'D': 500, 'M': 1000}
+        roman = {'I': 1, 'V': 5, 'X': 10,  'L': 50, 'C':  100, 'D': 500, 'M': 1000}
         result = 0
 
         for i in range(len(s)):
