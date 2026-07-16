01006caf52e2dd9ed01a985bbb54404843a20754
Soyeon <111319810+ssoyeee@users.noreply.github.com>
2026-07-15 22:41:28 -0400

    LC-jump-game [Runtime: 43 ms; Memory: 13.3 MB]

--- array/jump-game.py
+++ array/jump-game.py
@@ -1,11 +1,13 @@
 class Solution(object):
     def canJump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
         max_reach = 0
         for i in range(len(nums)):
             if i > max_reach:
                 return False
             max_reach = max(max_reach, nums[i] + i)
         return True
-
-        # T: O(n)
-        # S: O(1)
+        
