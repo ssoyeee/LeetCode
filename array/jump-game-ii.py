dcc33c0edc8408a8d2e8769629598a152b94dab3
Soyeon <111319810+ssoyeee@users.noreply.github.com>
2026-07-22 21:24:15 -0400

    LC-jump-game-ii [Runtime: 7 ms; Memory: 13.1 MB]

--- array/jump-game-ii.py
+++ array/jump-game-ii.py
@@ -1,16 +1,21 @@
-class Solution:
-    def jump(self, nums: List[int]) -> int:
-        # T: O(N)
-        # S: O(1)
+class Solution(object):
+    def jump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        # The starting range of the first jump is [0, 0]
+        answer, n = 0, len(nums)
+        cur_end, cur_far = 0, 0
 
-        jumps = 0
-        current_end = 0
-        max_reach = 0
+        for i in range(n - 1):
+            # Update the farthest reachable index of this jump.
+            cur_far = max(cur_far, i + nums[i])
 
-        for i in range(len(nums)-1): # don't need to check the last index, as we are already arrived
-            max_reach = max(max_reach, nums[i]+i)
-            if i == current_end:
-                current_end = max_reach
-                jumps += 1
-        return jumps
+            # If we finish the starting range of this jump,
+            # Move on to the starting range of the next jump.
+            if i == cur_end:
+                answer += 1
+                cur_end = cur_far
 
+        return answer        
