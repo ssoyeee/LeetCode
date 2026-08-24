bfee4e2c3a3457360c3bef45e5ebe4db22e42e46
Soyeon <111319810+ssoyeee@users.noreply.github.com>
2026-08-24 13:52:42 -0400

    LC-total-distance-traveled [Runtime: 8 ms; Memory: 19.3 MB]

--- math/total-distance-traveled.py
+++ math/total-distance-traveled.py
@@ -1,4 +1,10 @@
 class Solution:
     def distanceTraveled(self, mainTank: int, additionalTank: int) -> int:
-        
-        return 10*(mainTank + min((mainTank-1)//4,additionalTank))
+        total = mainTank # total liter 
+        while mainTank >= 5:
+            mainTank -= 5
+            if additionalTank > 0:
+                additionalTank -= 1
+                mainTank += 1
+                total += 1
+        return total * 10
