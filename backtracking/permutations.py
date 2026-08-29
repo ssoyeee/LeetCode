9aa6092e4794f3d49c0508c2f9d57a2253766678
Soyeon <111319810+ssoyeee@users.noreply.github.com>
2026-08-29 14:55:24 -0400

    LC-permutations [Runtime: 3 ms; Memory: 19.3 MB]

--- backtracking/permutations.py
+++ backtracking/permutations.py
@@ -8,3 +8,6 @@ class Solution:
                 yield from helper(prefix + [num], nums[:index] + nums[index+1:]) 
                 # add num to prefix, all elements except the one at index
         return list(helper([], nums))
+        
+        # Time: O(N!*N)
+        # Space: O(N!*N) The recursion stack depth is O(n), but since each call also creates a new sliced list of size up to O(n), the total auxiliary space becomes O(n^2). Including the output, which holds n! permutations of length n, the overall space complexity is O(n! * n)
