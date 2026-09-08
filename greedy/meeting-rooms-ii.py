09da144c38b7d053cda83191512abf1c3bc93555
Soyeon <111319810+ssoyeee@users.noreply.github.com>
2026-09-07 23:44:08 -0400

    LC-meeting-rooms-ii [Runtime: 2 ms; Memory: 21.3 MB]

--- greedy/meeting-rooms-ii.py
+++ greedy/meeting-rooms-ii.py
@@ -7,8 +7,10 @@ class Solution:
         intervals.sort(key=lambda x: x[0])
         heap = []
         for start, end in intervals:
-            if heap and heap[0] <= start:
+            if heap and heap[0] <= start: #room is already free
                 heapq.heapreplace(heap, end)
-            else:
+            else: # new room is needed, push a new room with new end time
                 heapq.heappush(heap, end)    
         return len(heap)
+        # Time: O(N log N) -- sort() is O(n log n); each heap operation is O(log n), done n times
+        # Space: O(n) -- in the worst case, heap size could be n
