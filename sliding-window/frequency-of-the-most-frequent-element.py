a3370ce03184b6dff8f965d6fef975345eac132a
Soyeon <111319810+ssoyeee@users.noreply.github.com>
2026-09-08 22:56:17 -0400

    LC-frequency-of-the-most-frequent-element [Runtime: N/A; Memory: N/A]

--- sliding-window/frequency-of-the-most-frequent-element.py
+++ sliding-window/frequency-of-the-most-frequent-element.py
@@ -1,13 +1,16 @@
 class Solution:
     def maxFrequency(self, nums: List[int], k: int) -> int:
         nums.sort()
-        l=0
-        max_freq=0
+        l = 0
+        max_freq = 0
+        window_sum = 0
         for r in range(len(nums)):
-            cost = nums[r]*(r-l+1)-sum(nums[l:r+1])
+            window_sum += nums[r]
+            cost = nums[r]*(r-l+1) - window_sum
             while cost > k:
-                l+=1
-                cost = nums[r]*(r-l+1)-sum(nums[l:r+1])
+                window_sum -= nums[l]
+                l += 1
+                cost = nums[r]*(r-l+1) - window_sum
             if cost <= k:
                 max_freq = r-l+1
         return max_freq
