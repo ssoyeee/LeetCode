e82445c7d3dce91e3f8224973759ffa030ddd795
Soyeon <111319810+ssoyeee@users.noreply.github.com>
2026-09-12 19:06:32 -0400

    LC-3sum [Runtime: N/A; Memory: N/A]

--- sorting/3sum.py
+++ sorting/3sum.py
@@ -19,9 +19,12 @@ class Solution(object):
                 elif total > 0:
                     right -= 1
                 else:
+                    output.append([nums[i], nums[left], nums[right]])
+                    left += 1
+                    right -= 1
                     while left < right and nums[left] == nums[left-1]:
                         left += 1
-                    while left < right and nums[right] == nums[right+1]
+                    while left < right and nums[right] == nums[right+1]:
                         right -= 1
 
         return output
