e32ed387d02d7022e2346c861cd877dd81771707
Soyeon <111319810+ssoyeee@users.noreply.github.com>
2026-09-12 19:06:38 -0400

    LC-3sum [Runtime: N/A; Memory: N/A]

--- two-pointers/3sum.py
+++ two-pointers/3sum.py
@@ -22,6 +22,7 @@ class Solution(object):
                     output.append([nums[i], nums[left], nums[right]])
                     left += 1
                     right -= 1
+                    
                     while left < right and nums[left] == nums[left-1]:
                         left += 1
                     while left < right and nums[right] == nums[right+1]:
